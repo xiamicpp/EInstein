81b3b224726fa43139ebd4d1daad60bb9daea863
xiamicpp <ltbuaa@163.com>
2018-05-10 23:45:58 +0800

    update

--- athena/src/main/java/org/einstein/athena/proto/parse/Parser.java
+++ athena/src/main/java/org/einstein/athena/proto/parse/Parser.java
@@ -19,21 +19,21 @@ import java.util.List;
 /**
  * @create by kevin
  **/
-public class Parser implements IParser<List<ITemplete>,String>{
+public class Parser implements IParser<List<ITemplete>, String> {
     private static Logger logger = LoggerFactory.getLogger(Parser.class);
     private BufferedReader reader = null;
     private String m_dir;
     private List<File> m_files = new ArrayList<>(10);
     private List<ITemplete> m_templetes;
     private ITemplete m_templete;
-    private static HashMap<String,IParser> parsers;
+    private static HashMap<String, IParser> parsers;
 
     static {
         parsers = new HashMap<>();
-        parsers.put("package",new PackageParser());
-        parsers.put("lineType",new ParserUtil());
-        parsers.put("@EProtoEntity",new ProtoEntityParser());
-        parsers.put("ProtoEntityDefine",new ProtoDefineParser());
+        parsers.put("package", new PackageParser());
+        parsers.put("lineType", new ParserUtil());
+        parsers.put("@EProtoEntity", new ProtoEntityParser());
+        parsers.put("ProtoEntityDefine", new ProtoDefineParser());
     }
 
 
@@ -43,10 +43,10 @@ public class Parser implements IParser<List<ITemplete>,String>{
         this.loadFiles();
         Iterator<File> it = this.m_files.iterator();
         this.m_templetes = new ArrayList<>();
-        while (it.hasNext()){
+        while (it.hasNext()) {
             File file = it.next();
             parseEProto(file);
-            this.reader =null;
+            this.reader = null;
         }
         return this.m_templetes;
     }
@@ -54,7 +54,7 @@ public class Parser implements IParser<List<ITemplete>,String>{
 
     private void loadFiles() throws ESynatx {
         File file = new File(this.m_dir);
-        if(!file.exists())
+        if (!file.exists())
             throw new ESynatx("File not exists");
         this.m_files = FileUtil.getFileList(file, ".java");
     }
@@ -64,8 +64,8 @@ public class Parser implements IParser<List<ITemplete>,String>{
             this.reader = new BufferedReader(new FileReader(file));
             parseContent();
         } catch (FileNotFoundException e) {
-            throw new ESynatx("Can not open eproto file",e);
-        }finally {
+            throw new ESynatx("Can not open eproto file", e);
+        } finally {
             try {
                 this.reader.close();
             } catch (IOException e) {
@@ -82,42 +82,59 @@ public class Parser implements IParser<List<ITemplete>,String>{
 
             while ((line = this.reader.readLine()) != null) {
                 line = StringUtils.trim(line);
-                switch (lineTypeParse.parse(line)){
-                    case " ": //skip blank
+                switch (lineTypeParse.parse(line)) {
+                    case BLANK: //skip blank
                         break;
-                    case "package":
-                        PackageParser packageParser = (PackageParser)parsers.get("package");
+                    case PACKAGE:
+                        PackageParser packageParser = (PackageParser) parsers.get("package");
                         templete.setM_packageName(packageParser.parse(line));
                         break;
-                    case "/*":
+                    case IMPORTRS:
+                        break;
+                    case COMMENT_START:
                         StringBuilder sb = new StringBuilder();
-                        while (!StringUtils.endsWith(line,"*/")){
+                        while (!StringUtils.endsWith(line, "*/")) {
                             sb.append(line);
                             line = this.reader.readLine();
                         }
                         sb.append(line);
                         templete.setM_comments(sb.toString());
                         break;
-                    case "@EProtoEntity":
+                    case COMMENT_BODY:
+                        break;
+                    case COMMENT_END:
+                        break;
+                    case CLASS_ANNOTATION:
                         ProtoEntityParser eProtoParser = (ProtoEntityParser) parsers.get("@EProtoEntity");
                         templete.setM_id(eProtoParser.parse(line));
                         break;
-                    case "public interface":
+                    case CLASS_HEADER:
                         ProtoDefineParser protoDefineParser = (ProtoDefineParser) parsers.get("ProtoEntityDefine");
                         ProtoDefineParser.ProtoHeader header = protoDefineParser.parse(line);
                         templete.setM_name(header.class_name);
                         templete.setM_extends(header.extends_);
                         break;
-                    case "@":  //proto field
+                    case PROPERTY_ANNOTATION:  //proto field
                         //TODO Persist annotation
-                       // while(StringUtils.startsWith("@"))
+                        // while(StringUtils.startsWith("@"))
+                        break;
+                    case PROPERTY:
+                        break;
+                    case ANNOTATION:
+                        break;
+                    case CLASS_END:
+                        break;
+                    case UNKNOWN:
+                        break;
+                    default:
+                        break;
 
                 }
             }
-        }catch (IOException e){
-            throw new ESynatx("file content read error",e);
+        } catch (IOException e) {
+            throw new ESynatx("file content read error", e);
         } catch (Exception e) {
-            throw new ESynatx("parse line failed",e);
+            throw new ESynatx("parse line failed", e);
         }
     }
 
--- athena/src/main/java/org/einstein/util/ParserUtil.java
+++ athena/src/main/java/org/einstein/util/ParserUtil.java
@@ -1,59 +1,68 @@
 package org.einstein.util;
-
-import org.apache.commons.lang3.StringUtils;
 import org.einstein.framework.IParser;
-import org.einstein.framework.ITemplete;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.*;
 import java.util.regex.Pattern;
 
 /**
  * @create by kevin
  **/
-public class ParserUtil implements IParser<String, String> {
-
-    private Logger logger = LoggerFactory.getLogger(ParserUtil.class);
-    private static final String TYPE_PACKAGE = "package";
-    private static final String TYPE_EPROTOENTITY = "@EProtoEntity";
-    private static final String TYPE_EPROTOFIELD = "@";
-    private static final String TYPE_IMPORT = "import";
-    private static final String TYPE_PUBLIC = "public";
-    private static final String TYPE_PRIVATE = "private";
-    private static final String TYPE_PROTECTED = "protected";
-    private static final String TYPE_INTERFACE = "interface";
-
+public class ParserUtil implements IParser<ParserUtil.LINETYPE, String> {
     private static final Pattern pattern_property = Pattern.compile("^[\\S]*\\s*[\\S]*\\s*=\\s*[\\S]*;");
     private static final Pattern pattern_package = Pattern.compile("^(package)\\s*[\\S]*;");
     private static final Pattern pattern_classHeader = Pattern.compile("^(public)\\s*(interface)\\s*[\\S]*\\s*(extends)?\\s*[\\s\\S]*\\{");
-    private static final Pattern pattern_classAnno = Pattern.compile("");
+    private static final Pattern pattern_classAnno = Pattern.compile("^@[\\S]*\\([\\s]*id[\\s]*=[\\s]*[1-9]\\d*[\\s]*,[\\s\\S]*\\)");
+    private static final Pattern pattern_import = Pattern.compile("^(import)[\\s]*[\\S]*;");
+    private static final Pattern pattern_blank = Pattern.compile("^[\\s]*");
+    private static final Pattern pattern_propertyAnno = Pattern.compile("^(@)[\\S]*\\([\\s\\S]*\\)");
+    private static final Pattern pattern_Comment_Start = Pattern.compile("^/\\*\\*");
+    private static final Pattern pattern_Comment_Body = Pattern.compile("^\\*[\\s\\S]*");
+    private static final Pattern pattern_Comment_End = Pattern.compile("^\\*\\*/");
+    private static final Pattern pattern_annotation = Pattern.compile("^\\\\[\\s\\S]*");
+    private static final Pattern pattern_classEnd = Pattern.compile("^}");
 
-
-
-    public String parse(String line) {
-       if(StringUtils.isBlank(line))
-           return " ";
-       if(StringUtils.startsWith(line,TYPE_PACKAGE))
-           return TYPE_PACKAGE;
-       if(StringUtils.startsWith(line,"/*"))
-           return "/*";
-       if(StringUtils.startsWith(line,"//"))
-           return "//";
-       if(StringUtils.startsWith(line,TYPE_IMPORT))
-           return TYPE_IMPORT;
-       if(StringUtils.startsWith(line,TYPE_EPROTOENTITY))
-           return TYPE_EPROTOENTITY;
-       if(StringUtils.startsWith(line,TYPE_EPROTOFIELD))
-           return TYPE_EPROTOFIELD;
-       if(StringUtils.startsWith(line,TYPE_PUBLIC+" "+TYPE_INTERFACE))
-           return TYPE_PUBLIC + " " +TYPE_INTERFACE;
-       return "UNKNOWN_";
+    public ParserUtil.LINETYPE parse(String line) {
+        if(pattern_blank.matcher(line).matches())
+            return LINETYPE.BLANK;
+        else if(pattern_propertyAnno.matcher(line).matches())
+            return LINETYPE.PROPERTY_ANNOTATION;
+        else if(pattern_property.matcher(line).matches())
+            return LINETYPE.PROPERTY;
+        else if(pattern_annotation.matcher(line).matches())
+            return LINETYPE.ANNOTATION;
+        else if(pattern_Comment_Body.matcher(line).matches())
+            return LINETYPE.COMMENT_BODY;
+        else if(pattern_Comment_Start.matcher(line).matches())
+            return LINETYPE.COMMENT_START;
+        else if(pattern_Comment_End.matcher(line).matches())
+            return LINETYPE.COMMENT_END;
+        else if(pattern_import.matcher(line).matches())
+            return LINETYPE.IMPORTRS;
+        else if(pattern_package.matcher(line).matches())
+            return LINETYPE.PACKAGE;
+        else if(pattern_classAnno.matcher(line).matches())
+            return LINETYPE.CLASS_ANNOTATION;
+        else if(pattern_classHeader.matcher(line).matches())
+            return LINETYPE.CLASS_HEADER;
+        else if(pattern_classEnd.matcher(line).matches())
+            return LINETYPE.CLASS_END;
+        else
+            return LINETYPE.UNKNOWN;
     }
 
 
     public enum LINETYPE{
-        BLANK,PROPERTY,CLASS_HEADER,PROPERTY_ANNOTATION,CLASS_ANNOTATION,CLASS_END,CLASS_COMMENT,FIELD_COMMENT;
+        UNKNOWN,
+        BLANK,
+        PROPERTY,
+        CLASS_HEADER,
+        PROPERTY_ANNOTATION,
+        CLASS_ANNOTATION,
+        CLASS_END,
+        ANNOTATION,
+        IMPORTRS,
+        PACKAGE,
+        COMMENT_START,
+        COMMENT_BODY,
+        COMMENT_END;
     }
 
 
