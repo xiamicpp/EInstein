b610d6e8e8b20d3cdc4b227ce6c1f199cd2d2df2
xiamicpp <ltbuaa@163.com>
2018-05-18 00:07:28 +0800

    fix travis

--- install-protobuf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://protobuf.googlecode.com/files/protobuf-3.5.1.tar.gz
-tar -xzvf protobuf-3.5.1.tar.gz
-cd protobuf-3.5.1 && ./configure --prefix=/usr && make && sudo make install
